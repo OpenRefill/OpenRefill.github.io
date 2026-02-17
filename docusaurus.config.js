f9e57cc9d411432ad78a0a7588699c4c240a9850
Francois_OpenRefill <hello@openrefill.com>
2026-02-17 16:21:29 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
   organizationName: 'OpenRefill', // Usually your GitHub org/user name.
   projectName: 'OpenRefill.github.io', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
